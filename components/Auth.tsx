50f596d2cbb75764fb559865108fc16c3dcbc315
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2024-10-31 00:00:35 +0530

    gluestack errors resolved and gluestack removed completely

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
 import { supabase } from "../lib/supabase";
-import { Button, Input } from "@rneui/themed";
 import { useRouter } from "expo-router";
 
 export default function Auth() {
