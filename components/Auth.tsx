ef61f631e614412089167d0c7bfde805ceb058ba
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2024-10-31 09:16:11 +0530

    rneui issues resolved

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
 import { supabase } from "../lib/supabase";
 import { useRouter } from "expo-router";
+import { Button, Input } from "@rneui/themed";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
